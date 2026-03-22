d75877e11d0277c1c7b348e3ad44d41892661132
Ishaan <ishaansinghsus@outlook.com>
2026-03-22 01:13:22 -0400

    Add project files.

---
+++ Todo Manager/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Todo_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listViewTasks = new System.Windows.Forms.ListView();
+            this.columnTask = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnToggleDone = new System.Windows.Forms.Button();
+            this.RCT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New task";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(420, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.label2.Location = new System.Drawing.Point(455, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Due date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(458, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 31);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(590, 32);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(36, 26);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // listViewTasks
+            // 
+            this.listViewTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnTask,
+            this.columnDue,
+            this.columnDone});
+            this.listViewTasks.FullRowSelect = true;
+            this.listViewTasks.GridLines = true;
+            this.listViewTasks.HideSelection = false;
+            this.listViewTasks.Location = new System.Drawing.Point(21, 78);
+            this.listViewTasks.Name = "listViewTasks";
+            this.listViewTasks.Size = new System.Drawing.Size(605, 280);
+            this.listViewTasks.TabIndex = 5;
+            this.listViewTasks.UseCompatibleStateImageBehavior = false;
+            this.listViewTasks.View = System.Windows.Forms.View.Details;
+            // 
+            // columnTask
+            // 
+            this.columnTask.Text = "Task";
+            this.columnTask.Width = 380;
+            // 
+            // columnDue
+            // 
+            this.columnDue.Text = "Due Date";
+            this.columnDue.Width = 140;
+            // 
+            // columnDone
+            // 
+            this.columnDone.Text = "Done";
+            this.columnDone.Width = 80;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnRemove.Location = new System.Drawing.Point(21, 370);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 30);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            // 
+            // btnToggleDone
+            // 
+            this.btnToggleDone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnToggleDone.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToggleDone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btnToggleDone.Location = new System.Drawing.Point(127, 370);
+            this.btnToggleDone.Name = "btnToggleDone";
+            this.btnToggleDone.Size = new System.Drawing.Size(120, 30);
+            this.btnToggleDone.TabIndex = 7;
+            this.btnToggleDone.Text = "Toggle Done";
+            this.btnToggleDone.UseVisualStyleBackColor = false;
+            // 
+            // RCT
+            // 
+            this.RCT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.RCT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RCT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.RCT.Location = new System.Drawing.Point(253, 370);
+            this.RCT.Name = "RCT";
+            this.RCT.Size = new System.Drawing.Size(239, 30);
+            this.RCT.TabIndex = 8;
+            this.RCT.Text = "Remove Completed Tasks";
+            this.RCT.UseVisualStyleBackColor = false;
+            this.RCT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(640, 420);
+            this.Controls.Add(this.RCT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listViewTasks);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnToggleDone);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Todo Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView listViewTasks;
+        private System.Windows.Forms.ColumnHeader columnTask;
+        private System.Windows.Forms.ColumnHeader columnDue;
+        private System.Windows.Forms.ColumnHeader columnDone;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnToggleDone;
+        private System.Windows.Forms.Button RCT;
+    }
+}
+
---
+++ Todo Manager/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Todo_Manager
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _storagePath;
+        private List<TodoItem> _tasks = new List<TodoItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // storage path: %APPDATA%\TodoManager\tasks.xml
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var dir = Path.Combine(appData, "TodoManager");
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+            _storagePath = Path.Combine(dir, "tasks.xml");
+
+            // wire events
+            btnAdd.Click += BtnAdd_Click;
+            btnRemove.Click += BtnRemove_Click;
+            btnToggleDone.Click += BtnToggleDone_Click;
+            listViewTasks.DoubleClick += ListViewTasks_DoubleClick;
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+
+            // small UX
+            textBox1.KeyDown += TextBox1_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasks();
+            RefreshListView();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasks();
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddCurrentTask();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            AddCurrentTask();
+        }
+
+        private void AddCurrentTask()
+        {
+            var desc = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(desc))
+            {
+                MessageBox.Show("Please enter a task description.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var item = new TodoItem
+            {
+                Description = desc,
+                DueDate = dateTimePicker1.Value.Date,
+                IsDone = false
+            };
+
+            _tasks.Add(item);
+            textBox1.Clear();
+            RefreshListView();
+            SaveTasks();
+        }
+
+        private void BtnRemove_Click(object sender, EventArgs e)
+        {
+            if (listViewTasks.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Select a task to remove.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var li = listViewTasks.SelectedItems[0];
+            int idx = li.Index;
+            if (idx >= 0 && idx < _tasks.Count)
+            {
+                _tasks.RemoveAt(idx);
+                RefreshListView();
+                SaveTasks();
+            }
+        }
+
+        private void BtnToggleDone_Click(object sender, EventArgs e)
+        {
+            ToggleSelectedDone();
+        }
+
+        private void ListViewTasks_DoubleClick(object sender, EventArgs e)
+        {
+            ToggleSelectedDone();
+        }
+
+        private void ToggleSelectedDone()
+        {
+            if (listViewTasks.SelectedItems.Count == 0)
+                return;
+
+            var li = listViewTasks.SelectedItems[0];
+            int idx = li.Index;
+            if (idx >= 0 && idx < _tasks.Count)
+            {
+                _tasks[idx].IsDone = !_tasks[idx].IsDone;
+                RefreshListView();
+                SaveTasks();
+            }
+        }
+
+        private void RefreshListView()
+        {
+            listViewTasks.BeginUpdate();
+            listViewTasks.Items.Clear();
+
+            for (int i = 0; i < _tasks.Count; i++)
+            {
+                var t = _tasks[i];
+                var lvi = new ListViewItem(t.Description);
+                lvi.SubItems.Add(t.DueDate.ToShortDateString());
+                lvi.SubItems.Add(t.IsDone ? "Yes" : "No");
+
+                // styling: strikeout if done, gray text
+                if (t.IsDone)
+                {
+                    lvi.ForeColor = Color.Gray;
+                    lvi.Font = new Font(listViewTasks.Font, FontStyle.Strikeout);
+                }
+                else
+                {
+                    // overdue highlight
+                    if (t.DueDate < DateTime.Today)
+                    {
+                        lvi.ForeColor = Color.DarkRed;
+                    }
+                    else
+                    {
+                        lvi.ForeColor = Color.Black;
+                    }
+                    lvi.Font = new Font(listViewTasks.Font, FontStyle.Regular);
+                }
+
+                listViewTasks.Items.Add(lvi);
+            }
+
+            listViewTasks.EndUpdate();
+        }
+
+        private void LoadTasks()
+        {
+            try
+            {
+                if (!File.Exists(_storagePath))
+                {
+                    _tasks = new List<TodoItem>();
+                    return;
+                }
+
+                using (var fs = File.OpenRead(_storagePath))
+                {
+                    var serializer = new XmlSerializer(typeof(List<TodoItem>));
+                    _tasks = (List<TodoItem>)serializer.Deserialize(fs) ?? new List<TodoItem>();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load tasks: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _tasks = new List<TodoItem>();
+            }
+        }
+
+        private void SaveTasks()
+        {
+            try
+            {
+                using (var fs = File.Create(_storagePath))
+                {
+                    var serializer = new XmlSerializer(typeof(List<TodoItem>));
+                    serializer.Serialize(fs, _tasks);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to save tasks: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // keep the auto-generated event present (unused)
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // no-op: date selected for new task
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _tasks.RemoveAll(i => i.IsDone);
+            RefreshListView();
+            SaveTasks();
+        }
+    }
+
+    [Serializable]
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
